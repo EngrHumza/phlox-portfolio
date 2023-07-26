e91c0393a3976f80752e7f61a7b21f4b83c0224c
unknown <humzaliaqat01@gmail.com>
2023-07-26 17:36:03 +0500

    'Completed'

--- js/app.js
+++ js/app.js
@@ -68,6 +68,78 @@ function reveal() {
   }
 }
 
+var windowHeight =  Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
+    lastTop;
+
+// window.addEventListener('scroll', function(event) {
+//   var train = document.getElementById("trainMotion"),
+//       top = train.getBoundingClientRect().top,
+//       offset = top - windowHeight;
+      
+//     // console.group(offset, top, train);
+
+
+//       if (offset > 0) {
+//       	train.classList.remove('choochoo');
+//       	return;
+//       }
+      
+//       if (top < windowHeight / 2 && top > lastTop) {
+//       	train.classList.remove('choochoo');
+//       }
+      
+//       if (train.className.indexOf('choocho') === -1 && top < lastTop) {
+//       	train.classList.add('choochoo');
+//       }
+      
+//       lastTop = top;
+     
+// });
+
+
+// window.addEventListener('scroll', function(event) {
+  // var windowHeight =  Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
+  //   lastTop;
+  // var train = document.getElementById('trainMotion');
+      // top = train.getBoundingClientRect().top,
+      // console.log(offset);
+      
+      // var windowHeight = window.innerHeight;
+      // var elementTop = train.getBoundingClientRect().top;
+      // var elementVisible = 150;
+// 
+      // var offset = elementTop - windowHeight;
+     
+      // if (elementTop < windowHeight - elementVisible) {
+        // train.classList.add("choochoo");
+      // } else {
+        // train.classList.remove("choochoo");
+      // }
+
+
+      // if (offset > 0) {
+      // 	train.classList.remove('choochoo');
+      // 	return;
+      // }
+
+      // console.log(offset);
+
+      // if(offset<0){
+      // 	train.classList.add('choochoo');
+      // }
+      
+      // if (top < windowHeight / 2 && top > lastTop) {
+      // 	train.classList.remove('choochoo');
+      // }
+      
+      // if (train.className.indexOf('choocho') === -1 && top < lastTop) {
+      // 	train.classList.add('choochoo');
+      // }
+      
+      // lastTop = top;
+    
+// });
+
 
 function hidemenu() {
   let navUl = document.getElementById("nav-ul");
@@ -81,3 +153,169 @@ function showmenu(){
   navUl.classList.remove("nav-items");
   navUl.classList.add("mb-menu");
 }
+
+
+// Chatgpt  animation
+
+document.addEventListener('DOMContentLoaded', function() {
+  let previousScroll = window.pageYOffset || document.documentElement.scrollTop;
+  const icons = document.querySelectorAll('.trainMotion svg');
+
+  window.addEventListener('scroll', function() {
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScroll > previousScroll) {
+      // Scrolling down, move icons left with animation delay
+      icons.forEach((icon, index) => {
+        icon.style.transition = `transform 0.5s ease-out ${index * 100}ms`; /* Adding animation delay */
+        icon.style.transform = 'translateX(50px)';
+      });
+    } else {
+      // Scrolling up, move icons right with animation duration and timing function
+      icons.forEach((icon, index) => {
+        icon.style.transition = 'transform 0.8s ease-in-out'; /* Adding animation duration and timing function */
+        icon.style.transform = 'translateX(-80px)';
+      });
+    }
+
+    previousScroll = currentScroll;
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  let previousScroll = window.pageYOffset || document.documentElement.scrollTop;
+  const icons = document.querySelectorAll('.icons');
+
+  // Convert NodeList to an array
+  const iconsArray = [...icons];
+
+  // Array to store animation properties for each icon
+  const animationProperties = iconsArray.map((icon, index) => ({
+    element: icon,
+    direction: index % 2 === 0 ? 'left' : 'right',
+    delay: `${0.1 * index}s`,
+  }));
+
+  window.addEventListener('scroll', function() {
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScroll > previousScroll) {
+      // Scrolling down
+      animationProperties.forEach((anim) => {
+        anim.element.style.transition = `transform 1s ease-out ${anim.delay}`;
+        anim.element.style.transform = anim.direction === 'left' ? 'translateX(-25px)' : 'translateX(25px)';
+      });
+    } else {
+      // Scrolling up
+      animationProperties.forEach((anim) => {
+        anim.element.style.transition = `transform 1s ease-out ${anim.delay}`;
+        anim.element.style.transform = anim.direction === 'left' ? 'translateX(25px)' : 'translateX(-25px)';
+      });
+    }
+
+    previousScroll = currentScroll;
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  let previousScroll = window.pageYOffset || document.documentElement.scrollTop;
+  const icons = document.querySelectorAll('.icons1');
+
+  // Convert NodeList to an array
+  const iconsArray = [...icons];
+
+  // Array to store animation properties for each icon
+  const animationProperties = iconsArray.map((icon, index) => ({
+    element: icon,
+    direction: index % 2 === 0 ? 'left' : 'right',
+    delay: `${0.1 * index}s`,
+  }));
+
+  window.addEventListener('scroll', function() {
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScroll > previousScroll) {
+      // Scrolling down
+      animationProperties.forEach((anim) => {
+        anim.element.style.transition = `transform 1s ease-out ${anim.delay}`;
+        anim.element.style.transform = anim.direction === 'left' ? 'translateX(-50px)' : 'translateX(50px)';
+      });
+    } else {
+      // Scrolling up
+      animationProperties.forEach((anim) => {
+        anim.element.style.transition = `transform 1s ease-out ${anim.delay}`;
+        anim.element.style.transform = anim.direction === 'left' ? 'translateX(50px)' : 'translateX(-50px)';
+      });
+    }
+
+    previousScroll = currentScroll;
+  });
+});
+
+
+
+// project Images
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  let previousScroll = window.pageYOffset || document.documentElement.scrollTop;
+  const icons = document.querySelectorAll('.auxImg img');
+
+  // Convert NodeList to an array
+  const iconsArray = [...icons];
+
+  // Array to store animation properties for each icon
+  const animationProperties = iconsArray.map((icon, index) => ({
+    element: icon,
+    direction: index % 2 === 0 ? 'left' : 'right',
+    delay: `${0.1 * index}s`,
+    rotation: 0, // Initial rotation angle
+  }));
+
+  const maxRotation = 5; // Maximum rotation in degrees
+
+  window.addEventListener('scroll', function() {
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScroll > previousScroll) {
+      // Scrolling down
+      animationProperties.forEach((anim) => {
+        anim.element.style.transition = `transform 1s ease-out ${anim.delay}`; /* Increase animation duration to 1s */
+        anim.rotation = Math.min(anim.rotation + 1, maxRotation); // Increment rotation (max 1 degree at a time)
+        applyRotation(anim);
+      });
+    } else {
+      // Scrolling up
+      animationProperties.forEach((anim) => {
+        anim.element.style.transition = `transform 1s ease-out ${anim.delay}`; /* Increase animation duration to 1s */
+        anim.rotation = Math.max(anim.rotation - 1, -maxRotation); // Decrement rotation (max 1 degree at a time)
+        applyRotation(anim);
+      });
+    }
+
+    previousScroll = currentScroll;
+  });
+
+  // Function to apply rotation to an element
+  function applyRotation(anim) {
+    const rotationString = `translateX(${anim.direction === 'left' ? '-5px' : '5px'}) rotate(${anim.rotation}deg)`;
+    anim.element.style.transform = rotationString;
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
